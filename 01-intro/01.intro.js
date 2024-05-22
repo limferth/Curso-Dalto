7eb633d8974f3fb6a4ad6e9412fcce5e3ce5cb51
limferth <limferth@gmail.com>
2024-05-22 15:52:54 -0400

    primer commit del curso con Dalto

---
+++ 01-intro/01.intro.js
@@ -0,0 +1,30 @@
+/*
+INTRODUCCIOB Que es Javascript
+    lenguaje de programacion
+    interpretado
+    orientadoa objetos
+    imperativo
+    case sensitive (Upper and lower case)
+    basado en prototipos / instancias / classless.
+    tipado debil
+    lenguaje dinamico
+
+    EMASCRIPT(5.1, 6, 7, 8, netx)
+2. para que se usa ?
+    Dinamismo de los sitios web
+    servicios en nodejs
+    tecnologias frontend
+
+        OTROS USOS NO COMUNES
+            Inteligencia artificial
+            placas electronicas (Jhonny five)
+            Mobile Apps
+            desktop Apps
+            Etc.
+3. VARIABLES = a un espacio que guardamos en memoria, 
+*/ 
+
+let recipiente = "papel";
+alert(papel)
+
+
---
+++ 01-intro/02.tipos.js
@@ -0,0 +1,26 @@
+/*
+TIPOS DE DATOS: (Cadena de tecto), number, boolean,
+CASOS ESPECIALES DE DATOS = Undefined, Null, Nan;
+Scope,
+Hosting = como funcionan las fases de creacion 
+
+*/
+let numero1 = null; // el valor esta vacio y le declaramos vacia
+let numero2 = undefined; // no esta inicializada, pero si esta declarada
+let numero = NaN; //NaN no es un numero (Not a Number)
+    
+/*
+2.  UQ==QUE ES EL PROMPT = A una api
+
+*/ 
+let nombre = prompt("decime tu nombre");
+/*
+TIPOS DE OPERADORES EN JAVASCRIPT
+Un operador es un simbolo especial queespecifica que debe efectuar una determinada operacion
+estas operaciones pueden ser:
+ASIGNACION
+ARITMETICAS
+CONDICIONALES(O LOGICOS)
+RELACIONALES (O DE COMPARACION)
+ENTRE OTRAS
+*/
