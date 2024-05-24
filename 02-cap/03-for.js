42923f65ad2daede9cc74551ee83abb8354fb009
limferth <limferth@gmail.com>
2024-05-24 00:44:15 -0400

    aprendimos sobre como funciona for

---
+++ 02-cap/03-for.js
@@ -0,0 +1,22 @@
+// for = es un bucle determinado se ejecutan donde los decimos tiene 3 partes
+// 1. declaramos e inicializamos
+//la condicion
+// determinamos si va aumentar o disminuir
+for (let i = 0; i < 6; i++) {
+    
+    if(i == 4) {
+        break;
+    }
+    console.log(i);
+}
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+//}
+for (let i = 0; i < 10; i++) {
+    
+    if(i == 4) {
+        continue;
+    }
+    console.log(i);
+}
