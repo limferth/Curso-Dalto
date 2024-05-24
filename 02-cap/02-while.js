3724c2696666a3f4d5f31b598ad3a6928022dbf1
limferth <limferth@gmail.com>
2024-05-24 00:43:16 -0400

    aprendimos sobre while

---
+++ 02-cap/02-while.js
@@ -0,0 +1,24 @@
+/*
+bucles e iteracion 
+    -Definicion y usos = 
+    */
+let numero = 0;
+
+while (numero <= 100) {
+    numero++;
+    console.log(numero)
+    if (numero == 10) {
+        break;
+    }
+};
+
+// DO WHILE primero ejecuta el codigo luego pregunta
+let number = 0;
+
+do {
+    
+    console.log(number)
+
+    number++;
+} while (number < 6);
+
